ea54a459954eb0e5c28396cdadf748978e0a8ba2
PiotrEliks <piotr.eliks@wp.pl>
2025-02-07 12:11:54 +0100

    Signup, login, logout updated
    Updated SignUpPage, LoginPage, Navbar and created signup, login, logout logic (frontend)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,7 @@ import ProfilePage from './pages/ProfilePage.jsx'
 import { axiosInstance } from './lib/axios.js'
 import { useAuthStore } from './store/useAuthStore.js'
 import { Loader } from 'lucide-react'
+import { Toaster } from 'react-hot-toast'
 
 const App = () => {
   const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
@@ -35,6 +36,7 @@ const App = () => {
         <Route path="/settings" element={<SettingsPage />}/>
         <Route path="/profile" element={ authUser ? <ProfilePage /> : <Navigate to="/login" /> }/>
       </Routes>
+      <Toaster />
     </div>
   )
 }
